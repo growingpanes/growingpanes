56094fbcca12003974c7b8ab1788bbad563e4c03
Andrew Cole <aocole@gmail.com>
2015-01-14 15:36:38 -0800

    README is a shell script

--- panesd/README.md
+++
@@ -1,3 +0,0 @@
-Start chrome with a command like:
-
-`google-chrome --remote-debugging-port=2345 --noerrdialogs --no-first-run --kiosk http://your-url.local --user-data-dir=/tmp/panesd-chrome-profile`
---
+++ panesd/README.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+# Start chrome with a command like:
+
+google-chrome --remote-debugging-port=2345 --noerrdialogs --no-first-run --kiosk about:blank --user-data-dir=/tmp/panesd-chrome-profile
