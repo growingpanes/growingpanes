bc719108e00a857c6f9da1f28fcb51c8fb05df0f
Andrew Cole <aocole@gmail.com>
2015-02-06 12:19:23 -0800

    better display setup script with CEC poweron

---
+++ ansible/roles/chrome/files/displaySetup.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+echo "on 0" | cec-client -s
+sleep 5
+xrandr --output DVI-D-0 --off --output HDMI-0 --mode 1920x1080 --pos 0x0 --rotate normal --output DVI-I-1 --mode 1920x1080 --pos 1920x1080 --rotate normal --output DVI-I-0 --off --output DP-1 --off --output DP-0 --mode 1920x1080 --pos 1920x0 --rotate normal
