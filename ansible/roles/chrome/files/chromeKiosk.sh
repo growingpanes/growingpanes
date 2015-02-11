e8ef37ee4634d9372a0ab7a3ff2d5a56c13a36a3
Andrew Cole <aocole@gmail.com>
2015-02-11 15:05:32 -0800

    Adjustments for private github repos

--- ansible/roles/chrome/files/chromeKiosk.sh
+++ ansible/roles/chrome/files/chromeKiosk.sh
@@ -2,6 +2,6 @@
 while :
 do
 	matchbox-window-manager &
-	google-chrome --remote-debugging-port=2345 --noerrdialogs --no-first-run --kiosk about:blank --start-maximized --user-data-dir=/tmp/panesd-chrome-profile
+	google-chrome --remote-debugging-port=2345 --noerrdialogs --no-first-run --kiosk http://localhost/presentations/next --start-maximized --user-data-dir=/tmp/panesd-chrome-profile
 	sleep 2s
 done
