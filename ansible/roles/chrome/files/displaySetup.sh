32da5a69f01a51444867037185ecbb438b3b459a
Andrew Cole <aocole@gmail.com>
2015-04-03 14:31:35 -0700

    Update CEC control to act on all attached TVs

--- ansible/roles/chrome/files/displaySetup.sh
+++ ansible/roles/chrome/files/displaySetup.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
-echo "on 0" | cec-client -s
+# turn all the TVs on and set to first HDMI input
+for a in `cec-client -l | grep 'com port:' | sed "s/com port:\\s*//"`; do 
+	echo "on 0" | cec-client -p 0 -s $a; 
+done
 sleep 5
 xrandr --output DVI-I-1 --off --output HDMI-0 --mode 1920x1080 --pos 0x0 --rotate normal --output DVI-D-0 --mode 1920x1080 --pos 1920x1080 --rotate normal --output DVI-I-0 --off --output DP-1 --off --output DP-0 --off
