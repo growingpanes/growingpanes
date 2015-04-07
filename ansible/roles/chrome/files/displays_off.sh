a5386d0bcc89881128b1ee5172099a78c736a06e
Andrew Cole <aocole@gmail.com>
2015-04-07 14:12:28 -0700

    minor ansible updates

---
+++ ansible/roles/chrome/files/displays_off.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# turn all the TVs on and set to first HDMI input
+for a in `cec-client -l | grep 'com port:' | sed "s/com port:\\s*//"`; do 
+	echo "standby 0" | cec-client -p 0 -s $a; 
+done
