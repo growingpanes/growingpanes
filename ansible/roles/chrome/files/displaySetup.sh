0dd4b5f2ec8c2000014fa29b6ee387ee4cc41a1c
Andrew Cole <aocole@gmail.com>
2015-04-09 12:36:19 -0700

    Make screen on/off happen in parallel

--- ansible/roles/chrome/files/displaySetup.sh
+++ ansible/roles/chrome/files/displaySetup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # turn all the TVs on and set to first HDMI input
 for a in `cec-client -l | grep 'com port:' | sed "s/com port:\\s*//"`; do 
-	echo "on 0" | cec-client -p 0 -s $a; 
+	echo "on 0" | cec-client -p 0 -s $a &
 done
 sleep 5
 xrandr \
