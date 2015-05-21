b09105a09093faa995470586789201ff3f9ea783
Andrew Cole <aocole@gmail.com>
2015-05-21 15:06:02 -0700

    Add chrome policy, disable gpu blacklist

--- ansible/roles/chrome/files/chromeKiosk.sh
+++ ansible/roles/chrome/files/chromeKiosk.sh
@@ -2,6 +2,6 @@
 while :
 do
 	matchbox-window-manager &
-	google-chrome --remote-debugging-port=2345 --noerrdialogs --no-first-run --kiosk http://localhost/presentations/next --start-maximized --user-data-dir=/tmp/panesd-chrome-profile --proxy-server="http://localhost:8118"
+	google-chrome --ignore-gpu-blacklist --remote-debugging-port=2345 --noerrdialogs --no-first-run --kiosk http://localhost/presentations/next --start-maximized --user-data-dir=/tmp/panesd-chrome-profile --proxy-server="http://localhost:8118"
 	sleep 2s
 done
