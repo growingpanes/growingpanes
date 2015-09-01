542e50c608f1a97e4fcc1dffbbc73cf4a68fa080
Andrew Cole <aocole@gmail.com>
2015-09-01 10:11:21 -0700

    Flip the top screens to reduce middle bezel width

--- ansible/roles/chrome/files/displaySetup.sh
+++ ansible/roles/chrome/files/displaySetup.sh
@@ -5,8 +5,8 @@ for a in `cec-client -l | grep 'com port:' | sed "s/com port:\\s*//"`; do
 done
 sleep 5
 xrandr \
-  --output DVI-I-1 --mode 1920x1080 --pos 1920x0 --rotate normal \
-  --output HDMI-0 --mode 1920x1080 --pos 0x0 --rotate normal \
+  --output DVI-I-1 --mode 1920x1080 --pos 1920x0 --rotate inverted \
+  --output HDMI-0 --mode 1920x1080 --pos 0x0 --rotate inverted \
   --output DVI-D-0 --mode 1920x1080 --pos 0x1080 --rotate normal \
   --output DVI-I-0 --off \
   --output DP-1 --mode 1920x1080 --pos 1920x1080 --rotate normal \
